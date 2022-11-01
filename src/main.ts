f2e3d54530d6e2c524fabb509be24a1a518a56ee
FonDitbul <niki7084@naver.com>
2022-11-01 23:01:29 +0900

    [COP-2] add : express-session 추가 (review x)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infrastructure/app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: '2022-11-01-coPang',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
